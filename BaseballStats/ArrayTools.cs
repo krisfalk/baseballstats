688f1244ca814168bffe763a1aa25555ff5de4c2
krisfalk <krisfalk2007@hotmail.com>
2016-08-10 15:27:20 -0500

    initial upload

---
+++ BaseballStats/ArrayTools.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballStats
+{
+    class ArrayTools
+    {
+        public double[] stats;
+        public string[] statNames;
+
+        public ArrayTools()
+        {
+            stats = new double[5];
+            statNames = new string[]{"Singles", "Doubles", "Triples", "Home Runs", "At Bat"};
+            fillStatArray();
+        }
+        public void fillStatArray()
+        {
+            for (int i = 0; i < statNames.Length; i++)
+            {
+                Console.WriteLine("Enter number for {0}", statNames[i]);
+                stats[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        public void CalculateSLG()
+        {
+            double final;
+
+            final = (stats[0] + (2 * stats[1]) + (3 * stats[2]) + (4 * stats[3])) / stats[4];
+            Console.WriteLine(final);
+        }
+        public void PrintArray(string[] stringArray, double[] intArray)
+        {
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+                Console.WriteLine(intArray[i]);
+            }
+        }
+    }
+}
---
+++ BaseballStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayTools myArray = new ArrayTools();
+            Console.Clear();
+            myArray.PrintArray(myArray.statNames, myArray.stats);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("This players slugging percentage is:");
+            myArray.CalculateSLG();
+            Console.ReadLine();
+        }
+    }
+}
